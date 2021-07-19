10c9ec18dcb6183f762d2c0cf4a5e35dd7d8c442
Piriya Chaigul <pryeditor@gmail.com>
2021-07-19 17:18:52 +0700

    Add Pic Files

---
+++ src/components/Information.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="information" class="section">
+    <div id="information-container" class="page-container">
+      <div class="left">
+        <h4>HI, I'm</h4>
+        <h1>PIRIYA CHAIGUL</h1>
+        <h3>I'm Third Year Frontend Developer at CPE KMUTT.</h3>
+      </div>
+      <div class="right">
+        <img id="pry-vector" src="../assets/piriya-vector-small.png" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+#information-container {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width:100%;
+}
+
+#pry-vector{
+  width: 350px;
+}
+</style>
---
+++ src/views/Mainpage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="mainpage">
+    <Information/>
+  </div>
+</template>
+
+<script>
+import Information from "../components/Information.vue"
+export default {
+  components:{
+    Information
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
